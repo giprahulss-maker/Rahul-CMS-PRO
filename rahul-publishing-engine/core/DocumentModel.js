caf15c92f8459d74729c1265e4107a3b24b7b2c0
giprahulss-maker <gip.rahulss@gmail.com>
2026-02-12 12:33:32 +0530

    Add DocumentModel class for document structure

---
+++ rahul-publishing-engine/core/DocumentModel.js
@@ -0,0 +1,15 @@
+export class DocumentModel {
+
+  constructor(){
+    this.data = {
+      type:"document",
+      meta:{
+        title:"Untitled",
+        author:"",
+        style:"vancouver"
+      },
+      children:[]
+    };
+  }
+
+}
